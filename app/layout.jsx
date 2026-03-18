942522a97dccdfb8b8fc59eb4c611bb0333c72d0
smba11 <simba.1w34@gmail.com>
2026-03-18 01:15:05 -0700

    Update layout.jsx

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,1 +1,14 @@
+import "./globals.css";
 
+export const metadata = {
+  title: "SMBAFLEX",
+  description: "Streaming Platform",
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body>{children}</body>
+    </html>
+  );
+}
